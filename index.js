c834de250400d33a785ce12ac1db56106aa28f25
Nick Cooper <nickcooper84@gmail.com>
2019-04-16 16:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+happyHolidaysTo('you')
+
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+happyHolidaysTo('Independence Day', 'you')
+
+function holidayCountdown(holiday, days) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
+
+holidayCountdown("Mother's Day", 20)
